e45848c32b296123802687baad9e1f3f75669bdf
BeomSu-Kang <kbs21c1987@gmail.com>
2022-09-03 17:57:08 +0900

    리듀서 사용해서 api요청학

--- api-integrate/src/App.js
+++ api-integrate/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Users from "./Users";
+import UsersReducer from "./UsersReducer";
 
 const App = () => {
   return (
     <div>
       <Users />
+      <hr />
+      <UsersReducer />
     </div>
   );
 };
