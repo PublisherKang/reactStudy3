359a5978c5e956fa45d7c18d5f865ed833e562b9
BeomSu-Kang <kbs21c1987@gmail.com>
2022-09-04 19:49:43 +0900

    리덕스 카운터 구현하기 완성

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -6,8 +6,9 @@ import reportWebVitals from "./reportWebVitals";
 import { createStore } from "redux";
 import rootReducer from "./modules";
 import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer); // 스토어를 만든다
+const store = createStore(rootReducer, composeWithDevTools()); // 스토어를 만든다
 // console.log(store.getState()); // 스토어 상태를 확인한다.
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
