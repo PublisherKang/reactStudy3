ab0e8960b8bbcd43fa0ad23722865f93bf92f537
BeomSu-Kang <kbs21c1987@gmail.com>
2022-09-07 17:01:15 +0900

    shallowEqual을 사용한 리랜더링 최적화

--- learn-redux/src/containers/CounterContainer.js
+++ learn-redux/src/containers/CounterContainer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import Counter from "../components/Counter";
 import { decrease, increase, setDiff } from "../modules/counter";
 
@@ -7,10 +7,13 @@ const CounterContainer = () => {
   // useSelect는 리덕스 스토어의 상태를 조회하는 Hook이다.
   // state의 최값은 store.getState()함수를 호출했을때 나타나는 결과물과 동일하다.
 
-  const { number, diff } = useSelector((state) => ({
-    number: state.counter.number,
-    diff: state.counter.diff,
-  }));
+  const { number, diff } = useSelector(
+    (state) => ({
+      number: state.counter.number,
+      diff: state.counter.diff,
+    }),
+    shallowEqual
+  );
 
   // useDispatch 는 리덕스 스토어의 dispatch 를 함수에서 사용 할 수 있게 해주는 Hook 이다.
   const dispatch = useDispatch();
